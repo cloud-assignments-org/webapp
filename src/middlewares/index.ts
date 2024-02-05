09d556a1f125cfe20aa8a9909a6c28763753ed98
parthasarathydNU <parthasarathy.d@northeastern.edu>
2024-02-05 02:06:33 -0500

    Project setup part II Integrated Jest, Tsoa, and TypeORM With working test cases

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,22 +1,22 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import { RegisterRoutes } from "../routes/routes.js";
-import {methodNotAllowed, badRequestHandler, cacheControl} from "./errorHandler.middleware.js";
+import {
+  methodNotAllowed,
+  badRequestHandler,
+  cacheControl,
+} from "./errorHandler.middleware.js";
 import { EnvConfiguration, Environment } from "../config/env.config.js";
 import swaggerJson from "../routes/swagger.json" assert { type: "json" };
 
 export const configMiddleware = (app: any) => {
   app.use(express.json(), cors());
-  app.use( cacheControl, methodNotAllowed, badRequestHandler );
+  app.use(cacheControl, methodNotAllowed, badRequestHandler);
 
   RegisterRoutes(app);
 
   if (EnvConfiguration.NODE_ENV === Environment.DEVELOPMENT) {
-    app.use(
-      "/api-docs",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerJson)
-    );
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
   }
 };
