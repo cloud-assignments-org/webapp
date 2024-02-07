128e5dc11f9d2c6161c9f5f0425c66bb8379ffdc
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-07 00:14:21 -0500

    Implementing basic auth middleware for user end points

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -10,11 +10,12 @@ import {
 import { EnvConfiguration, Environment } from "../config/env.config.js";
 import swaggerJson from "../routes/swagger.json" assert { type: "json" };
 import { errorHandler } from "../errorHandling/ErrorHandler.js";
+import { basicAuthMiddleware } from "./basicAuth.middleware.js";
 
 export const configMiddleware = (app: any) => {
   app.use(express.json(), cors());
   app.use(cacheControl, methodNotAllowed, badRequestHandler, errorHandler);
-
+  app.use(basicAuthMiddleware);
   RegisterRoutes(app);
 
   if (EnvConfiguration.NODE_ENV === Environment.DEVELOPMENT) {
