9361619021c233e306dcb74f6ce7c5a56f858690
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-02-20 14:12:53 -0800

    fixing node installation

--- packer/scripts/nodeJsInstallation.sh
+++ packer/scripts/nodeJsInstallation.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
-# Define Node.js version and installation directory
+echo "Checking for wget and installing if not present..."
+if ! command -v wget &> /dev/null; then
+    echo "wget could not be found. Installing wget..."
+    sudo yum install -y wget
+fi
+
+echo "Setting up node and other dependencies"
+
+# Define Node.js version and directory variables
 NODE_VERSION=v20.2.0
 NODE_DIST=node-$NODE_VERSION-linux-x64
 INSTALL_DIR=/usr/local/lib/nodejs
 
-# Download Node.js specific version
 echo "Downloading Node.js version $NODE_VERSION..."
-wget https://nodejs.org/dist/$NODE_VERSION/$NODE_DIST.tar.xz
+if wget https://nodejs.org/dist/$NODE_VERSION/$NODE_DIST.tar.xz; then
+    echo "Installing Node.js version $NODE_VERSION..."
 
-# Create target directory if it doesn't exist
-sudo mkdir -p $INSTALL_DIR
+    # Create the target directory and extract the Node.js tarball
+    sudo mkdir -p $INSTALL_DIR
+    sudo tar -xJf $NODE_DIST.tar.xz -C $INSTALL_DIR
 
-# Extract Node.js and install
-echo "Installing Node.js version $NODE_VERSION..."
-sudo tar -xJvf $NODE_DIST.tar.xz -C $INSTALL_DIR
-
-# Setup environment variables
-ENV_FILE=/etc/profile.d/nodejs.sh
-echo "Setting up environment variables..."
-sudo tee $ENV_FILE <<EOF
+    # Set up the environment variables
+    echo "Setting up environment variables..."
+    ENV_FILE=/etc/profile.d/nodejs.sh
+    sudo tee $ENV_FILE <<EOF
 export PATH=$INSTALL_DIR/$NODE_DIST/bin:\$PATH
 EOF
 
-# Make the environment script executable
-sudo chmod +x $ENV_FILE
+    # Apply the environment variables for this session
+    source $ENV_FILE
 
-# Apply environment variables
-source $ENV_FILE
+    # Verify the installation
+    echo "Node.js version:"
+    node -v
+    echo "npm version:"
+    npm -v
 
-# Verify installation
-echo "Node.js version:"
-node -v
-echo "npm version:"
-npm -v
-
-echo "Node.js $NODE_VERSION installation completed."
+    echo "Node.js $NODE_VERSION installation completed."
+else
+    echo "Failed to download Node.js $NODE_VERSION"
+fi
