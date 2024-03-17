007c608f1ed500cf7dc8c1ac3bea709c1780c15c
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-17 15:55:41 -0400

    adding logs to index .ts file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { EnvConfiguration } from "./config/env.config.js";
 import createApp from "./app.js";
 import { checkDBConnectionRepeatedly } from "./utils/dbConnectivity.util.js";
+import logger from "./logger.js";
 class Server {
   constructor() {
     this.bootstrap();
@@ -11,6 +12,9 @@ class Server {
   async bootstrap() {
     checkDBConnectionRepeatedly();
     const app = createApp();
+    logger.info("Created app");
+    logger.error("Created app");
+    logger.debug("Created app");
     app.listen(EnvConfiguration.PORT, () => {
       console.log("TCP server established on port ", EnvConfiguration.PORT);
     });
