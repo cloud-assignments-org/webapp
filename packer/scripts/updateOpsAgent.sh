954cd5ad66bc5f7f20c4bfac7892d37c38a9ac3f
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-20 12:38:36 -0400

    Removing time zone from logger messages

--- packer/scripts/updateOpsAgent.sh
+++ packer/scripts/updateOpsAgent.sh
@@ -15,7 +15,7 @@ logging:
     my-app-processor:
       type: parse_json
       time_key: time
-      time_format: "%Y-%m-%dT%H:%M:%S.%S%Z"
+      time_format: "%Y-%m-%dT%H:%M:%S"
     change_severity:
       type: modify_fields
       fields:
