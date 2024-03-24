0584cdeeb96d082922d8deec41696acb00d68d19
Dhruv Parthasarathy <parthasarathy.d@northeastern.edu>
2024-03-24 12:14:41 -0400

    Fixed flow of email validation

--- src/errorHandling/ErrorHandler.ts
+++ src/errorHandling/ErrorHandler.ts
@@ -13,24 +13,24 @@ import {
 // noinspection JSUnusedLocalSymbols
 export const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   if (error instanceof ValidateError) {
-    logMessage("Validation Error", "Error handler middleware", error.message, Severity.ERROR);
+    logMessage("Input Validation Error", "Error handler middleware", error.message || "Input validation error", Severity.ERROR);
     res.status(400).end();
   } else if (error instanceof NotFoundError) {
-    logMessage("Not Found Error", "Errr handler middleware", error.message, Severity.ERROR);
+    logMessage("Not Found Error", "Errr handler middleware", error.message || "No error message sent", Severity.ERROR);
     res.status(HTTPStatusCode.OK).end();
   } else if (error instanceof BadInputError) {
-    logMessage("Bad Input Error", "Error handler middleware", error.message, Severity.ERROR);
+    logMessage("Bad Input Error", "Error handler middleware", error.message || "No error message sent", Severity.ERROR);
     res.status(HTTPStatusCode.BAD_REQUEST).end();
   } else if (error instanceof BadRequestError) {
-    logMessage("Bad Request Error", "Error handler middleware", error.message, Severity.ERROR);
+    logMessage("Bad Request Error", "Error handler middleware", error.message || "No error message sent", Severity.ERROR);
     res.status(HTTPStatusCode.BAD_REQUEST).end();
   } else if (error instanceof AuthError) {
-    logMessage("Auth Error", "Error handler middleware", error.message, Severity.ERROR);
+    logMessage("Auth Error", "Error handler middleware", error.message || "No error message sent", Severity.ERROR);
     res.status(HTTPStatusCode.UNAUTHORIZED).end();
   } else if (error instanceof ExpiredTokenError) {
     res.status(HTTPStatusCode.FORBIDDEN).end();
   } else {
-    logMessage("Internal server error", "Error handler middleware", error.message, Severity.ERROR);
+    logMessage("Internal server error", "Error handler middleware", error.message || "No error message sent", Severity.ERROR);
     res.status(HTTPStatusCode.INTERNAL_SERVER_ERROR).end();
   }
 };
